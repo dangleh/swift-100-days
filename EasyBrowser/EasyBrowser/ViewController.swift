0712b2ca3c3d884e3d19d952cc138be5cf2e3b2a
DangLe <lehaidangdev@gmail.com>
2023-05-07 11:37:47 +0700

    Update EasyBrowser project

--- EasyBrowser/EasyBrowser/ViewController.swift
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
+    var websites = ["apple.com", "hackingwithswift.com"]
     var webView: WKWebView!
+    var progressView: UIProgressView!
     
     override func loadView() {
         webView = WKWebView()
@@ -23,6 +25,86 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        // We shoud embed 'Navigation Controller' for this line works.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        //
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        //
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let reload = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        let goBack = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: webView, action: #selector(webView.goBack))
+        let goForward = UIBarButtonItem(image: UIImage(systemName: "chevron.forward"), style: .plain, target: webView, action: #selector(webView.goForward))
+        toolbarItems = [goBack, spacer, goForward, progressButton ,spacer, reload]
+        navigationController?.isToolbarHidden = false
+        // Observe webview's load progress
+        // Register observe by keyPath
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    // Update toolbar items' enabled/disabled state based on webview's navigation history
+    func updateToolbarItems() {
+        // Enable/disable the "Go Back" button based on whether the webview can go back
+        if webView.canGoBack {
+            toolbarItems?[0].isEnabled = true
+        } else {
+            toolbarItems?[0].isEnabled = false
+        }
+        
+        // Enable/disable the "Go Forward" button based on whether the webview can go forward
+        if webView.canGoForward {
+            toolbarItems?[2].isEnabled = true
+        } else {
+            toolbarItems?[2].isEnabled = false
+        }
+    }
+
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .actionSheet)
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        ac.addAction(UIAlertAction(title:"Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://\(action.title!)")!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+        updateToolbarItems()
+    }
+    
+    // implement observerValue method after register observer
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        let ac = UIAlertController(title: "Blocked!", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        decisionHandler(.cancel)
     }
 }
 
+// Key-value observing KVO (observer)
+// Delegation
