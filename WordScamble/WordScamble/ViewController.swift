52f1938077627bc75b98d3995840f82a290cfcb7
Le Hai Dang <lehaidangdev@gmail.com>
2023-05-09 20:26:12 +0700

    initilize WordScrable project

---
+++ WordScamble/WordScamble/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WordScamble
+//
+//  Created by LHD on 09/05/2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
